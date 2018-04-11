9866e04b4e170d6abcc39969046093ad0ce39ed4
DroidFreak32 <rushabshah32@gmail.com>
2018-04-11 05:35:11 +0530

    Wall Done

--- CG/Proj.cpp
+++ CG/Proj.cpp
@@ -5,17 +5,17 @@ using namespace std;
 
 void init()
 {
-    glClearColor(1.0,1.0,1.0,1.0);
+    glClearColor(129/255.0f,212/255.0f,250/255.0f,1.0);
     glClear(GL_COLOR_BUFFER_BIT);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluOrtho2D(0,1000,1000,0); // Left Right Bottom Top
+    gluOrtho2D(0,1000,0,1000); // Left Right Bottom Top
     glFlush();
 }
 //glutDisplayFunc(display_func_name) mandatory to invoke any function
 
 void clear_screen(){
-    glClearColor(1.0,1.0,1.0,1.0);
+    glClearColor(3/255.0f,155/255.0f,229/255.0f,1.0);
     glClear(GL_COLOR_BUFFER_BIT);
 }
 void drawSquare(int x, int y){
@@ -49,6 +49,102 @@ void drawPolygon(int x, int y){
     glFlush();
 
 }
+
+//copy poly for wall
+void drawWall(){
+
+    // clear_screen();
+    // Base
+    glColor3ub(203,65,84);
+    glBegin(GL_POLYGON);
+        glVertex2d(250,0);
+        glVertex2d(250,300);
+        glVertex2d(1000,300);
+        glVertex2d(1000,0);
+        glVertex2d(250,0);
+    glEnd();
+
+    // Base Brick Left
+    glColor3ub(140,43,55);
+    glBegin(GL_POLYGON);
+        glVertex2d(300,200);
+        glVertex2d(300,250);
+        glVertex2d(400,250);
+        glVertex2d(400,200);
+        glVertex2d(300,200);
+    glEnd();
+    glBegin(GL_POLYGON);
+        glVertex2d(415,200);
+        glVertex2d(415,250);
+        glVertex2d(515,250);
+        glVertex2d(515,200);
+        glVertex2d(415,200);
+    glEnd();
+    glBegin(GL_POLYGON);
+        glVertex2d(350,135);
+        glVertex2d(350,185);
+        glVertex2d(450,185);
+        glVertex2d(450,135);
+        glVertex2d(350,135);
+    glEnd();
+
+    // Base Brick Right
+    glBegin(GL_POLYGON);
+        glVertex2d(950,200);
+        glVertex2d(950,250);
+        glVertex2d(850,250);
+        glVertex2d(850,200);
+        glVertex2d(950,200);
+    glEnd();
+    glBegin(GL_POLYGON);
+        glVertex2d(835,200);
+        glVertex2d(835,250);
+        glVertex2d(735,250);
+        glVertex2d(735,200);
+        glVertex2d(835,200);
+    glEnd();
+    glBegin(GL_POLYGON);
+        glVertex2d(900,135);
+        glVertex2d(900,185);
+        glVertex2d(800,185);
+        glVertex2d(800,135);
+        glVertex2d(900,135);
+    glEnd();
+
+    //Granite
+    glColor3ub(162,162,161);
+    glBegin(GL_POLYGON);
+        glVertex2d(200,300);
+        glVertex2d(200,325);
+        glVertex2d(1000,325);
+        glVertex2d(1000,300);
+        glVertex2d(200,300);
+    glEnd();
+
+    // Granite bricks
+    glColor3ub(77,77,77);
+    glLineWidth(3);
+    glBegin(GL_LINES);
+        glVertex2d(300,300);
+        glVertex2d(300,325);
+        glVertex2d(400,300);
+        glVertex2d(400,325);
+        glVertex2d(500,300);
+        glVertex2d(500,325);
+        glVertex2d(600,300);
+        glVertex2d(600,325);
+        glVertex2d(700,300);
+        glVertex2d(700,325);
+        glVertex2d(800,300);
+        glVertex2d(800,325);
+        glVertex2d(900,300);
+        glVertex2d(900,325);
+    glEnd();
+
+    glFlush();
+
+}
+
 void drawTriangle(int x, int y){
     clear_screen();
     printf("Triangle initial Coordinates are: %d\t%d\n",x,y);
@@ -95,7 +191,9 @@ void menuActions(int value){
         exit(0);
 
 }
- void display(){}
+ void display(){
+    drawWall();
+ }
 int main(int argc,char *argv[]){
     glutInit(&argc,argv);
     glutInitDisplayMode(GLUT_RGB);
